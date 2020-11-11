f8aa73109ea4bff4829b3d9fdc7878658719108e
JoyceOnor <72040357+JoyceOnor@users.noreply.github.com>
2020-11-11 22:21:59 +0100

    DELETE und PUT Tests hinzugefügt

---
+++ test/film/rest/filme-PUT.spec.ts
@@ -0,0 +1,305 @@
+/*
+ * Copyright (C) 2016 - present Juergen Zimmermann, Hochschule Karlsruhe
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { FilmGenre, Studio } from '../../../src/film/entity';
+import { HttpMethod, agent, createTestserver } from '../../testserver';
+import { HttpStatus, logger, serverConfig } from '../../../src/shared';
+import { afterAll, beforeAll, describe, test } from '@jest/globals';
+import fetch, { Headers, Request } from 'node-fetch';
+import type { AddressInfo } from 'net';
+import { PATHS } from '../../../src/app';
+import type { Server } from 'http';
+import chai from 'chai';
+import { login } from '../../login';
+
+const { expect } = chai;
+
+// IIFE (= Immediately Invoked Function Expression) statt top-level await
+// https://developer.mozilla.org/en-US/docs/Glossary/IIFE
+(async () => {
+    // startWith(), endWith()
+    const chaiString = await import('chai-string');
+    chai.use(chaiString.default);
+})();
+
+// -----------------------------------------------------------------------------
+// T e s t d a t e n
+// -----------------------------------------------------------------------------
+const geaenderterFilm: object = {
+    // filmnr wird nicht geaendet
+    titel: 'Geaendert',
+    bewertung: 1,
+    genre: FilmGenre.ACTION,
+    studio: Studio.UNIVERSAL,
+    preis: 33.33,
+    rabatt: 0.033,
+    lieferbar: true,
+    datum: '2016-02-03',
+    homepage: 'https://test.te',
+    regisseure: [{ nachname: 'Gamma', vorname: 'Claus' }],
+    schlagwoerter: ['JAVASCRIPT', 'TYPESCRIPT'],
+};
+const idVorhanden = '00000000-0000-0000-0000-000000000003';
+
+const geaendertesFilmIdNichtVorhanden: object = {
+    titel: 'Nichtvorhanden',
+    bewertung: 1,
+    genre: FilmGenre.ACTION,
+    studio: Studio.UNIVERSAL,
+    preis: 33.33,
+    rabatt: 0.033,
+    lieferbar: true,
+    datum: '2016-02-03',
+    regisseure: [{ nachname: 'Gamma', vorname: 'Claus' }],
+    schlagwoerter: ['JAVASCRIPT', 'TYPESCRIPT'],
+};
+const idNichtVorhanden = '00000000-0000-0000-0000-000000000999';
+
+const geaenderterFilmInvalid: object = {
+    titel: 'Alpha',
+    bewertung: -1,
+    genre: 'UNSICHTBAR',
+    studio: 'NO_STUDIO',
+    preis: 0.01,
+    rabatt: 0,
+    lieferbar: true,
+    datum: '12345-123-123',
+    filmNr: 'falsche-FilmNr',
+    regisseure: [{ nachname: 'Test', vorname: 'Theo' }],
+    schlagwoerter: [],
+};
+
+const veralteterFilm: object = {
+    // filmnr wird nicht geaendet
+    titel: 'Veraltet',
+    bewertung: 1,
+    genre: FilmGenre.ACTION,
+    studio: Studio.UNIVERSAL,
+    preis: 33.33,
+    rabatt: 0.033,
+    lieferbar: true,
+    datum: '2016-02-03',
+    homepage: 'https://test.te',
+    regisseure: [{ nachname: 'Gamma', vorname: 'Claus' }],
+    schlagwoerter: ['JAVASCRIPT', 'TYPESCRIPT'],
+};
+
+// -----------------------------------------------------------------------------
+// T e s t s
+// -----------------------------------------------------------------------------
+const path = PATHS.filme;
+let server: Server;
+let filmeUri: string;
+let loginUri: string;
+
+// Test-Suite
+describe('PUT /filme/:id', () => {
+    // Testserver starten und dabei mit der DB verbinden
+    beforeAll(async () => {
+        server = await createTestserver();
+
+        const address = server.address() as AddressInfo;
+        const baseUri = `https://${serverConfig.host}:${address.port}`;
+        filmeUri = `${baseUri}${path}`;
+        logger.info(`filmeUri = ${filmeUri}`);
+        loginUri = `${baseUri}${PATHS.login}`;
+    });
+
+    afterAll(() => { server.close() });
+
+    test('Vorhandenes Film aendern', async () => {
+        // given
+        const token = await login(loginUri);
+        const headers = new Headers({
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json',
+            'If-Match': '"0"',
+        });
+        const body = JSON.stringify(geaenderterFilm);
+        const request = new Request(`${filmeUri}/${idVorhanden}`, {
+            method: HttpMethod.PUT,
+            headers,
+            body,
+            agent,
+        });
+
+        // when
+        const response = await fetch(request);
+
+        // then
+        expect(response.status).to.be.equal(HttpStatus.NO_CONTENT);
+        const responseBody = await response.text();
+        expect(responseBody).to.be.empty;
+    });
+
+    test('Nicht-vorhandener Film aendern', async () => {
+        // given
+        const token = await login(loginUri);
+        const headers = new Headers({
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json',
+            'If-Match': '"0"',
+        });
+        const body = JSON.stringify(geaendertesFilmIdNichtVorhanden);
+        const request = new Request(`${filmeUri}/${idNichtVorhanden}`, {
+            method: HttpMethod.PUT,
+            headers,
+            body,
+            agent,
+        });
+
+        // when
+        const response = await fetch(request);
+
+        // then
+        expect(response.status).to.be.equal(HttpStatus.PRECONDITION_FAILED);
+        const responseBody = await response.text();
+        expect(responseBody).to.be.equal(
+            `Es gibt kein Film mit der ID "${idNichtVorhanden}".`,
+        );
+    });
+
+    test('Vorhandenes Film aendern, aber mit ungueltigen Daten', async () => {
+        // given
+        const token = await login(loginUri);
+        const headers = new Headers({
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json',
+            'If-Match': '"0"',
+        });
+        const body = JSON.stringify(geaenderterFilmInvalid);
+        const request = new Request(`${filmeUri}/${idVorhanden}`, {
+            method: HttpMethod.PUT,
+            headers,
+            body,
+            agent,
+        });
+
+        // when
+        const response = await fetch(request);
+
+        // then
+        expect(response.status).to.be.equal(HttpStatus.BAD_REQUEST);
+        const {genre, bewertung, studio, datum, filmNr } = await response.json();
+        expect(genre).to.be.equal(
+            'Das Genre eines Filmes muss ACTION oder SCIENCE_FICTION sein.',
+        );
+        expect(bewertung).to.endWith('eine gueltige Bewertung.');
+        expect(studio).to.be.equal(
+            'Das Studio eines Filmes muss UNIVERSAL oder DISNEY sein.',
+        );
+        expect(datum).to.contain('ist kein gueltiges Datum');
+        expect(filmNr).to.endWith('ist keine gueltige FILM-Nummer.');
+    });
+
+    test('Vorhandener Film aendern, aber ohne Versionsnummer', async () => {
+        // given
+        const token = await login(loginUri);
+        const headers = new Headers({
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json',
+        });
+        const body = JSON.stringify(geaenderterFilm);
+        const request = new Request(`${filmeUri}/${idVorhanden}`, {
+            method: HttpMethod.PUT,
+            headers,
+            body,
+            agent,
+        });
+
+        // when
+        const response = await fetch(request);
+
+        // then
+        expect(response.status).to.be.equal(HttpStatus.PRECONDITION_REQUIRED);
+        const responseBody = await response.text();
+        expect(responseBody).to.be.equal('Versionsnummer fehlt');
+    });
+
+    test('Vorhandener Film aendern, aber mit alter Versionsnummer', async () => {
+        // given
+        const token = await login(loginUri);
+        const headers = new Headers({
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json',
+            'If-Match': '"-1"',
+        });
+        const body = JSON.stringify(veralteterFilm);
+        const request = new Request(`${filmeUri}/${idVorhanden}`, {
+            method: HttpMethod.PUT,
+            headers,
+            body,
+            agent,
+        });
+
+        // when
+        const response = await fetch(request);
+
+        // then
+        expect(response.status).to.be.equal(HttpStatus.PRECONDITION_FAILED);
+        const responseBody = await response.text();
+        expect(responseBody).to.have.string('Die Versionsnummer');
+    });
+
+    test('Vorhandener Film aendern, aber ohne Token', async () => {
+        // given
+        const headers = new Headers({
+            'Content-Type': 'application/json',
+            'If-Match': '"0"',
+        });
+        const body = JSON.stringify(geaenderterFilm);
+        const request = new Request(`${filmeUri}/${idVorhanden}`, {
+            method: HttpMethod.PUT,
+            headers,
+            body,
+            agent,
+        });
+
+        // when
+        const response = await fetch(request);
+
+        // then
+        expect(response.status).to.be.equal(HttpStatus.UNAUTHORIZED);
+        const responseBody = await response.text();
+        expect(responseBody).to.be.equalIgnoreCase('unauthorized');
+    });
+
+    test('Vorhandener Film aendern, aber mit falschem Token', async () => {
+        // given
+        const token = 'FALSCH';
+        const headers = new Headers({
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'application/json',
+            'If-Match': '"0"',
+        });
+        const body = JSON.stringify(geaenderterFilm);
+        const request = new Request(`${filmeUri}/${idVorhanden}`, {
+            method: HttpMethod.PUT,
+            headers,
+            body,
+            agent,
+        });
+
+        // when
+        const response = await fetch(request);
+
+        // then
+        expect(response.status).to.be.equal(HttpStatus.UNAUTHORIZED);
+        const responseBody = await response.text();
+        expect(responseBody).to.be.equalIgnoreCase('unauthorized');
+    });
+});
